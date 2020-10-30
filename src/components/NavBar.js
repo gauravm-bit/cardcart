0db8090a487fdc1d0a914d1dcd0cf476b117203b
gauravm-bit <gauravwrk01@gmail.com>
2020-10-30 21:47:46 +0530

    ADDED: navbar for the list

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar, Badge } from 'react-bootstrap';
+
+class NavBar extends React.Component {
+
+    state = {
+        badgeCount: 0  
+    }
+
+
+
+    render() {
+
+        return <div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    Cart-Example
+                    <Badge variant="secondary"> {this.state.badgeCount} </Badge>
+                </Navbar.Brand>
+            </Navbar>
+        </div>
+    }
+}
+
+export default NavBar;
+
