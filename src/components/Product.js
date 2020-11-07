ee5da5f15ad23630222fe7069f24acb22206b7f4
gauravm-bit <gauravwrk01@gmail.com>
2020-11-07 12:40:47 +0530

    REFACTORED:disabled button prop

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,6 @@ import "./Product.css";
 
 class Product extends React.Component {
   render() {
-   
     return (
       <div>
         <div>
@@ -13,13 +12,31 @@ class Product extends React.Component {
           ) : (
             <Badge variant="primary m-2"> {this.props.product.value} </Badge>
           )}
-          <Button variant="dark m-2" size="sm" onClick={()=>{this.props.onIncrement(this.props.product)}}>
+          <Button
+            variant="dark m-2"
+            size="sm"
+            onClick={() => {
+              this.props.onIncrement(this.props.product);
+            }}
+          >
             {" "}
             +{" "}
           </Button>
-          <Button variant="dark m-2" size="sm" onClick={()=>{this.props.onDecrement(this.props.product)}}>
-            -
-          </Button>
+          {this.props.product.value === 0 ? (
+            <Button variant="dark m-2" size="sm" disabled>
+              -
+            </Button>
+          ) : (
+            <Button
+              variant="dark m-2"
+              size="sm"
+              onClick={() => {
+                this.props.onDecrement(this.props.product);
+              }}
+            >
+              -
+            </Button>
+          )}
           <Button variant="danger m-2" size="sm" onClick={this.props.deleteTag}>
             Delete
           </Button>
